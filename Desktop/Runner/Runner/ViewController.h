4f8264bdc1de890ae58685d47cc5529fecbf9964
chungdow <chungdow@gmail.com>
2014-10-06 11:35:49 +0800

    first commit

---
+++ Desktop/Runner/Runner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runner
+//
+//  Created by 王 從道 on 2014/10/6.
+//  Copyright (c) 2014年 王 從道. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Desktop/Runner/Runner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Runner
+//
+//  Created by 王 從道 on 2014/10/6.
+//  Copyright (c) 2014年 王 從道. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
